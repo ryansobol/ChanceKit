d02f082b8dc1d43132a8023739531b29b94a36aa
Ryan Sobol <hello@ryansobol.com>
2019-10-04 09:44:39 -0700

    Refactor to XCTAssertEqual(expected:,actual:)

--- Tests/LexerTests.swift
+++ Tests/LexerTests.swift
@@ -9,7 +9,7 @@ class LexerTests: XCTestCase {
       let expectedTokens = fixture.withTokens
       let actualTokens = lexed(parenthesis: token, into: tokens)
 
-      XCTAssertTrue(expectedTokens.count == actualTokens.count)
+      XCTAssertEqual(expectedTokens.count, actualTokens.count)
 
       for (expected, actual) in zip(expectedTokens, actualTokens) {
         XCTAssertTrue(expected.isEqualTo(actual), "expected: \(expected), actual: \(actual)")
@@ -24,7 +24,7 @@ class LexerTests: XCTestCase {
       let expectedTokens = fixture.withTokens
       let actualTokens = lexed(operator: token, into: tokens)
 
-      XCTAssertTrue(expectedTokens.count == actualTokens.count)
+      XCTAssertEqual(expectedTokens.count, actualTokens.count)
 
       for (expected, actual) in zip(expectedTokens, actualTokens) {
         XCTAssertTrue(expected.isEqualTo(actual), "expected: \(expected), actual: \(actual)")
@@ -39,7 +39,7 @@ class LexerTests: XCTestCase {
       let expectedTokens = fixture.withTokens
       let actualTokens = try! lexed(integer: integer, into: tokens)
 
-      XCTAssertTrue(expectedTokens.count == actualTokens.count)
+      XCTAssertEqual(expectedTokens.count, actualTokens.count)
 
       for (expected, actual) in zip(expectedTokens, actualTokens) {
         XCTAssertTrue(expected.isEqualTo(actual), "expected: \(expected), actual: \(actual)")
