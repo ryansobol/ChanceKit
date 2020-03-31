a3fad7e3342895fc323b1dfd99501bd99cb30636
Ryan Sobol <hello@ryansobol.com>
2020-03-31 12:11:53 -0700

    Remove comment

--- Tests/Fixtures/EvaluatableClosedRangeFixture.swift
+++ Tests/Fixtures/EvaluatableClosedRangeFixture.swift
@@ -8,7 +8,6 @@ typealias EvaluatableClosedRangeFixture = (
   value: ClosedRange<Int>
 )
 
-//  TODO: Handle 1d4, 1d6, 1d8, 1d10, 1d12, 1d20, 1d100, and 1d
 let evaluatableClosedRangeFixtures: [EvaluatableClosedRangeFixture] = [
   (
     description: "1d4",
