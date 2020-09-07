c3a8dac11820d9a457a86698a66f3f0d60a0b41e
Ryan Sobol <hello@ryansobol.com>
2020-09-07 11:04:58 -0700

    Add ExpressionError TODO cases

--- ChanceKit/ExpressionError.swift
+++ ChanceKit/ExpressionError.swift
@@ -1,17 +1,25 @@
 public enum ExpressionError: Error, Equatable {
+//  TODO: Refactor into the following error case
+//  case divisionByZero(operandLeft: String)
   case divisionByZero
+//  TODO: Refactor into the following error case
+//  case invalidInput(lexeme: String)
   case invalidLexeme(String)
+//  TODO: Refactor into the following error case
+//  case invalidCombination(operandLeft: String, operandRight: String)
   case invalidCombinationOperands(String, String)
   case missingParenthesisClose
   case missingParenthesisOpen
   case missingOperand
+//  TODO: Refactor into the following error case
+//  case missingRollSides(operand: String)
   case missingOperandRollSides
   case missingOperator
-//  TODO: Split into the following error cases
-//  case overflowAdditionOperands(Int, Int)
-//  case overflowDivisionOperands(Int, Int)
-//  case overflowMultiplicationOperands(Int, Int)
-//  case overflowNegationOperand(Int)
-//  case overflowSubtractionOperands(Int, Int)
+//  TODO: Refactor into the following error cases
+//  case overflowAddition(operandLeft: String, operandRight: String)
+//  case overflowDivision(operandLeft: String, operandRight: String)
+//  case overflowMultiplication(operandLeft: String, operandRight: String)
+//  case overflowNegation(operand: String)
+//  case overflowSubtraction(operandLeft: String, operandRight: String)
   case operationOverflow
 }
